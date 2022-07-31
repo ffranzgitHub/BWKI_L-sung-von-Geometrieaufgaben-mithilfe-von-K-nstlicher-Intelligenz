274aa7f532b58358f3c0656b815d16157d74b906
Levithan7 <68614985+Levithan7@users.noreply.github.com>
2022-07-31 09:57:47 +0200

    Fehler behoben
    Weil wir gleichzeitig an dem Projekt (oder ich nach Franz) gearbeitet haben, sind ein paar Fehler aufgetreten. Unter anderem musste ich in meinem Code nun die Dateinamen der .json Datei umändern.

--- test_codesnippets/json_test_loader.py
+++ test_codesnippets/json_test_loader.py
@@ -1,7 +1,6 @@
 import json
-#from Vorverarbeitung.convert_value_to_NE_regex import named_entities
 
-with open("Aufgaben und Kategorien/Aufgaben_Serlo_Json.json", "r") as read_file:
+with open("Daten Laden/Aufgaben_Serlo_Json.json", "r") as read_file:
     aufgaben = json.load(read_file)
 
     all_words = []
@@ -12,19 +11,9 @@ with open("Aufgaben und Kategorien/Aufgaben_Serlo_Json.json", "r") as read_file:
 
         new_json.append(aufgabe)
         aufgaben_text = aufgabe["Text"]
-        #named_entities(aufgaben_text) --> auskommentiert, weil es Probleme mit dem Import der named_entities() funltion gab
 
         for word in aufgaben_text.split(" "):
             if word not in all_words:
                 all_words.append(word)
         if aufgabe["Aufgabentyp"] not in classes:
             classes.append(aufgabe["Aufgabentyp"])    
-
-    #print(new_json)    --> falls wir die Listen um die Aufgaben entfernen wollen
-    #print(all_words)
-    #print(classes)
-
-with open("Aufgaben und Kategorien/Aufgaben_Serlo_Json.json", "w") as write_file:
-    json.dump(new_json, write_file, indent=4)
-
-    #print(emps[0][0].get("Ja-Nein Frage"))
