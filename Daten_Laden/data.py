76ea75031cc6f4bdb66285adc8efd416a083eb58
ffranzgitHub <franz.schlicht@gmail.com>
2022-08-06 00:42:29 +0200

    random auswahl der Daten für den training/val/test split

--- Daten_Laden/data.py
+++ Daten_Laden/data.py
@@ -1,13 +1,24 @@
+from numpy import unique
 from globale_Variablen import globale_variablen
 import json
 from Daten_Laden.dataset import aufgabenDataset
+import random
 
 def __add_split(data):
     '''die Funktion verändert die Json Variable selbst
-    
     Args:
         data: (Json List,dic) 
     '''
+    # sorted_data erstellen mit {"Aufgabentpy1": ["text1", "text2"], "Aufgabentyp2", [text3, text4]}
+    classes = unique([d.get("Aufgabentyp") for d in data])
+    sorted_data = {}
+    for aufgabe in classes:
+        sorted_data[aufgabe] = []
+    for datenpunkt in data:
+        sorted_data[datenpunkt["Aufgabentyp"]].append(datenpunkt["Text"])
+
+    # The split must be random and include all classes equaly in all splits
+    random.shuffle(data)
     for dic in data[:18]:
         dic["split"] = "train"
     for dic in data[18:21]:
