c36f0db8abc93ea932bb57313e18144c49e7d0dc
Levithan7 <68614985+Levithan7@users.noreply.github.com>
2022-08-07 16:10:29 +0200

    Stopwords und Training
    Ich habe in die stemming.py eine Liste mit Stopwords implementiert, sodass diese von nun an rausgefiltert werden, falls wir die stemming.py noch für unsere Daten aufbereitungs nutzen.
    
    Außerdem habe ich die Trainingsschleife erstellt

--- Vorverarbeitung/stemming.py
+++ Vorverarbeitung/stemming.py
@@ -2,10 +2,16 @@ import nltk
 from nltk.stem.porter import PorterStemmer
 import numpy as np
 
+
 #nltk.download("punkt")
 # Beim ersten ausführen des Scriptes
 
 stemmer = PorterStemmer()
+
+def get_stopswords():
+    with open("Vorverarbeitung/german_stopwords.txt") as f:
+        stop_words = [line for line in f if not line.startswith(";")].extend(["!", "?", "°"])
+    return stop_words
 
 def tokenize(sentence):
     return nltk.word_tokenize(sentence)
@@ -19,6 +25,6 @@ def bag_of_words(tokenized_sentence, all_words):
     return np.array([1.0 if word in tokenized_sentence else 0.0 for word in all_words], dtype=np.float32)
 
 def ignore_stop_words(word_list):
-    stop_words = ["!", "?", "°"]
+    stop_words = get_stopswords()
     word_list = [word for word in word_list if word not in stop_words]
     return word_list
