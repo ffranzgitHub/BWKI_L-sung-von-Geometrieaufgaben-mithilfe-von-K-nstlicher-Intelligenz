2585107eed6a85b290b72e6eda7c0f2a4b9d91c6
ffranzgitHub <franz.schlicht@gmail.com>
2022-08-07 17:06:25 +0200

    weiter an __add_split gearbeitet

--- Daten_Laden/data.py
+++ Daten_Laden/data.py
@@ -1,11 +1,24 @@
-from numpy import unique
-from globale_Variablen import globale_variablen
-import json
-from Daten_Laden.dataset import aufgabenDataset
-import random
+
+if __name__=="__main__":
+    from numpy import unique
+    import json
+    import random
+
+    globale_variablen = \
+        {
+            "split_train": 0.8,
+            "split_val": 0.1,
+            "split_test": 0.1
+        }
+else:
+    from numpy import unique
+    from globale_Variablen import globale_variablen
+    import json
+    from Daten_Laden.dataset import aufgabenDataset
+    import random
 
 
-def __verhaeltnisse_berechnen(sorted_data):
+def __verhaeltnisse2anzahl(sorted_data):
     '''rechnet aus den Verhältnissen der Splits und der Anzahl der Elemente in einer Klasse die konkrete Aufteilung aus'''
     gesammtlaenge = 0
     for array in sorted_data.values(): gesammtlaenge += len(array)  #Gesammtlänge der Daten
@@ -13,16 +26,17 @@ def __verhaeltnisse_berechnen(sorted_data):
     #Split Verhältnisse(p) zu Anzahlen(n)
 
     # Verhältniss bei ungeraden Zahlen -> test und val werden aufgerundet, train nimmt den Rest
-    test_n = round(len(data)*globale_variablen.get("split_test")+0.5) #rundet immer auf
-    val_n = round(len(data)*globale_variablen.get("split_val")+0.5)
+    test_n = round(gesammtlaenge*globale_variablen.get("split_test")+0.5) #rundet immer auf
+    val_n = round(gesammtlaenge*globale_variablen.get("split_val")+0.5)
     train_n = gesammtlaenge-test_n-val_n   #nimmt den Rest
 
     # Anzahl(n) der Elemente der Klassen aus den Verhältnissen(p) in den Splits berechen
 
-    n_classen = []
-    for _, class_list in sorted_data.items():   #Verhältnisse der Klassen berechnen
-        n_classen.append(len(class_list))
+
+    n_classen = [len(cls_list) for cls_list in sorted_data.values()] #Verhältnisse der Klassen
     p_classen = [n_classe/gesammtlaenge for n_classe in n_classen]
+
+
 
     #Anzahl der Elemente von den Klassen in den Splits
     #für alle train
@@ -40,10 +54,19 @@ def __verhaeltnisse_berechnen(sorted_data):
     test_n_classen = []
     for p_class, n_class, n_train, n_val in zip(p_classen, n_classen, train_n_classen, val_n_classen):
         if n_class-n_train-n_val > 0:
-            test_n_classen.append(n_c1_test = n_class-n_train-n_val)
+            test_n_classen.append(n_class-n_train-n_val)
         else:
             test_n_classen.append(1)
             n_train -= 1
+
+    n_dic = \
+        {
+            train_n: train_n_classen,
+            val_n: val_n_classen,
+            test_n: test_n_classen,
+        }
+    #TODO: funktioniert nicht
+    return n_dic
 
 def __add_split(data):
     '''die Funktion fügt jedem Datenpunkt einem Split (train/val/test) hinzu
@@ -57,7 +80,7 @@ def __add_split(data):
     
     [random.shuffle(liste) for liste in sorted_data.values()]   #zufällig
 
-    __verhaeltnisse_berechnen()
+    __verhaeltnisse2anzahl(sorted_data)
     #TODO: weiter anpassen
     #Verhältniss der Klassen in einem Split gleichhalten
 '''
@@ -86,9 +109,11 @@ def create_dataset():
 if __name__=="__main__":
     data = \
         {
-            "k1": [0, 1, 2],
-            "k2": [3, 4, 5, 6],
-            "k3": [7, 8]
+            "k1": [0, 1, 2, 3, 4, 3, 4, 5],
+            "k2": [3, 4, 5, 6, 4, 5, 6],
+            "k3": [7, 8, 9, 10, 34, 4, 4]
         }
     
-    __verhaeltnisse_berechnen(data)
+    dic = __verhaeltnisse2anzahl(data)
+
+    print(dic)
